5cc9e7fa3af845074fb816a458978404fb4bae64
KoolmanKoder <michael.koomler@oneidaihla.org>
2022-12-01 21:31:01 -0700

    overloading code by Michael For my beloved teacher Mr. Gross

---
+++ overloading.java
@@ -0,0 +1,43 @@
+// overloading code by Michael
+//For my beloved teacher Mr. Gross
+//On 12/1/2022
+
+
+
+public class overloading {
+    
+    public static void main(String[] args) {}
+     
+    int x  void add (5,0);
+   
+    static int add (int a, int b){
+    System.out.print("this is jerry. he is:");
+     return a + b;}
+      
+      static int add (int a, int b, int c){
+     System.out.print("this is mat. he is:");
+     return a + b + c;}
+        
+         static int add (int a, int b, int c, int d){
+     System.out.print("this is betty. She is:");
+     return a + b + c + d;}
+     
+      static int add (int a, int b, int c, int d, int e){
+     System.out.print("this is mary. She is:");
+     return a + b + c + d + e;}
+     
+    static int add (int a, int b, int c ,int d, int e, int f){
+    System.out.print("this is jerry jr. he is:");
+     return a + b + c + d + e + f;}
+  
+
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
